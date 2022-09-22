56078bbece34ff090a97f7edd5ea5e002eced5cf
IsaelGabriel <isaelgabrielfaria@gmail.com>
2022-09-22 13:45:37 -0300

    update

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -6,7 +6,7 @@ st.title("Activity Manager")
 st.header("Metas")
 st.subheader("Suas metas")
 
-solo_goal = Goal()
+solo_goal = Goal("test_goal", "Teste feito para atribuir valor", "g_1")
 solo_goal.get_card(st=st)
 
 st.subheader("Metas coletivas")
