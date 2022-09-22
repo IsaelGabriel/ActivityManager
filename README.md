aa21e00da2aaf628bad7adbf04268af6fa4d4648
IsaelGabriel <isaelgabrielfaria@gmail.com>
2022-09-22 13:15:11 -0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ActivityManager
+# Conceitos básicos de Streamlit
+## Prof. Massaki Igarashi
+<a href="mailto:prof.massaki@gmail.com">e-mail: prof.massaki@gmail.com</a>
---
+++ webapp00.py
@@ -0,0 +1,28 @@
+# myFirstStreamlitApp.py
+  
+#import the library
+import streamlit as st
+import math as m
+import numpy as np
+import pandas as p
+
+# Use st.title("") para adicionar um TÍTULO ao seu Web app
+st.title("Bananinha")
+
+# Use st.header("") para adicionar um CABEÇALHO ao seu Web app
+st.header("1")
+
+# Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
+st.subheader("2")
+
+# Use st.write("") para adicionar um texto ao seu Web app
+st.write("-_-")
+
+with st.sidebar:
+  st.write("Account")
+  st.markdown("[Account](account)")
+  st.write("Groups")
+  st.write("Goals")
+  st.write("Calendar")
+  st.write("Chat")
+  
