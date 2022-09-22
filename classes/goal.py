ff594fbaea227fb907d7f7801dd67e5b695b62d4
IsaelGabriel <isaelgabrielfaria@gmail.com>
2022-09-22 13:59:01 -0300

    update

--- classes/goal.py
+++ classes/goal.py
@@ -1,9 +1,21 @@
+import json
+
 class Goal:
-    def __init__(self, label, description,id):
-        self.label = label
-        self.description = description
-        self.id = id
+    def __init__(self,g_id):
+        v = json.loads("../json/goals.json")
+        self.g_id = g_id
+        if g_id in v.keys():
+            self.values = v[g_id]
+        else:
+            self.valid = False
 
     def get_card(self,st):
-        st.latex(self.label)
-        st.write(self.description)
+        st.subheader(self.values["label"])
+        st.write(self.values["description"])
+    
+    def update_goal(self,quantity):
+        v = json.loads("../json/goals.json")
+        self.values["current"] += quantity
+        #v[self.g_id] = self.values
+        v.update(self.values)
+        json.dumsp(v)
