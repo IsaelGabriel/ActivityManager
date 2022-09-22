da14273cab54f318aea6c24db683caf019725091
IsaelGabriel <isaelgabrielfaria@gmail.com>
2022-09-22 13:38:38 -0300

    update

---
+++ classes/goal.py
@@ -0,0 +1,6 @@
+class Goal:
+    def __init__(self):
+        pass
+
+    def get_card(self,st):
+        return st.write("ok")
--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,8 +1,11 @@
-# myFirstStreamlitApp.py
-  
-#import the library
 import streamlit as st
+
+from classes.goal import Goal
 
 st.header("Metas")
 st.subheader("Suas metas")
-st.subheader("Metas do grupo")
+
+solo_goal = Goal()
+solo_goal.get_card(st=st)
+
+st.subheader("Metas coletivas")
