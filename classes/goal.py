021d331fade364972f0a5c1cbc49ed96b8f274a6
IsaelGabriel <isaelgabrielfaria@gmail.com>
2022-09-22 14:04:14 -0300

    update

--- classes/goal.py
+++ classes/goal.py
@@ -2,7 +2,7 @@ import json
 
 class Goal:
     def __init__(self,g_id):
-        v = json.loads(open("goals.json", "r").read())
+        v = json.loads(open("goals.json", "r"))
         self.g_id = g_id
         if g_id in v.keys():
             self.values = v[g_id]
@@ -14,7 +14,7 @@ class Goal:
         st.write(self.values["description"])
     
     def update_goal(self,quantity):
-        v = json.loads(open("goals.json", "r").read())
+        v = json.loads(open("goals.json", "r"))
         self.values["current"] += quantity
         #v[self.g_id] = self.values
         v.update(self.values)
