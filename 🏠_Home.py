6a1e17e7b8b80f4525c3241e0d1612648087f466
IsaelGabriel <isaelgabrielfaria@gmail.com>
2022-09-22 14:23:42 -0300

    update

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,6 +1,15 @@
 import streamlit as st
 from classes.goal import Goal
 
+
+st.set_page_config(
+    page_title = "Activity Manager",
+    page_icon = None,
+    layout = "centered",
+    initial_sidebar_state = "auto"
+    menu_items = None
+)
+
 st.title("Activity Manager")
 st.header("Suas metas")
 
