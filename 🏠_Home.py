eac0da204e113fa2dffcbf13ce5e0286d19aba2f
IsaelGabriel <isaelgabrielfaria@gmail.com>
2022-09-22 14:41:33 -0300

    update

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -11,7 +11,7 @@ st.set_page_config(
 )
 
 goals = json.load(open("classes/goals.json", "r"))
-solo_goals, team_goals = []
+solo_goals, team_goals = [],[]
 
 for k in goals.keys():
     t = goals[k]["type"] 
