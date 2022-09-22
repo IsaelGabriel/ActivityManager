e74aba77dcab29af6eb447886f4d5cfc934394e4
IsaelGabriel <isaelgabrielfaria@gmail.com>
2022-09-22 13:21:38 -0300

    update

--- app.py
+++ app.py
@@ -2,27 +2,6 @@
   
 #import the library
 import streamlit as st
-import math as m
-import numpy as np
-import pandas as p
 
-# Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Bananinha")
+st run [entrypoint home.py]
 
-# Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("1")
-
-# Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("2")
-
-# Use st.write("") para adicionar um texto ao seu Web app
-st.write("-_-")
-
-with st.sidebar:
-  st.write("Account")
-  st.markdown("[Account](account)")
-  st.write("Groups")
-  st.write("Goals")
-  st.write("Calendar")
-  st.write("Chat")
-  
